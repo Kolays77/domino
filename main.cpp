b2a7293bc08863d842818c6f91a2d66a1a4b9ed2
Kolays77 <nimakarchuk@gmail.com>
2023-01-17 10:52:51 +0300

    Perf up 2x

--- main.cpp
+++ main.cpp
@@ -1,23 +1,59 @@
- #include "domino.h"
+#include <map>
+#include <utility>
+#include <vector>
+#include <cstdint>
+#include <bitset>
+#include <queue>
 
 #include <bitset>
 #include <iostream>
 #include <stdexcept>
 #include <string>
 #include <array>
-
 #include <thread>
 
+#include <bit>
 
-// g++ main.cpp -O2 -pthread --std=c++20
+// g++ main.cpp -Ofast -std=c++20  -funroll-loops
+
+std::bitset<28> make_bitset(std::vector<int>&& v) {
+    std::bitset<28> b;
+    for (int vv : v) b[vv] = true;
+    return b; 
+}
+
+const std::vector<std::bitset<28>> MAP_VALUE_NUMBER({make_bitset({0, 1, 2, 3, 4, 5, 6}),
+                                                     make_bitset({1, 7, 8, 9, 10, 11, 12 }),
+                                                     make_bitset({2, 8, 13, 14, 15, 16, 17 }),
+                                                     make_bitset({3, 9, 14, 18, 19, 20, 21 }),
+                                                     make_bitset({4, 10, 15, 19, 22, 23, 24 }),
+                                                     make_bitset({5, 11, 16, 20, 23, 25, 26 }),
+                                                     make_bitset({6, 12, 17, 21, 24, 26, 27 })});
+
+const std::vector<std::pair<int, int>> MAP_NUMBER_VALUES ({{0, 0},{0, 1},{0, 2},{0, 3},
+                                                           {0, 4},{0, 5},{0, 6},{1, 1}, 
+                                                           {1, 2},{1, 3},{1, 4},{1, 5},
+                                                           {1, 6},{2, 2},{2, 3},{2, 4},
+                                                           {2, 5},{2, 6},{3, 3},{3, 4},
+                                                           {3, 5},{3, 6},{4, 4},{4, 5},
+                                                           {4, 6},{5, 5},{5, 6},{6, 6}});
+
+class Timer {
+    std::chrono::time_point<std::chrono::steady_clock> timePoint;
+    size_t value;
+public:
+    void start() { timePoint = std::chrono::steady_clock::now(); }
+    void finish() {
+        auto curr = std::chrono::steady_clock::now();    
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(curr - timePoint);
+        value = elapsed.count();
+    }
+    size_t operator()() const { return value; }
+};
 
 
-std::vector<std::string> global_store;
-using namespace std::chrono_literals;
-
-int choose_pos(std::pair<int, int> dom, int last_number) {
-    if (dom.first == last_number)  return dom.second;
-    else return dom.first;
+inline int choose_pos(const std::pair<int, int>& dom, int last_number) {
+    return (dom.first == last_number) ? dom.second : dom.first;
 }
 
 struct GlobalState {
@@ -27,17 +63,12 @@ struct GlobalState {
     std::vector<long long unsigned> count_;
 
     int avaible = 0;
-    
-    GlobalState() : dominoes({}), last_numbers({}) {};
+
+    // GlobalState() : dominoes({}), last_numbers({}) {};
 
     GlobalState(int domino, int last_number) : dominoes(std::vector<int>(28, -1)), 
                                                last_numbers(std::vector<int>(28, -1)),
                                                count_(std::vector<long long unsigned>(28, 0)) {
-        
-        if (MAP_NUMBER_VALUES.at(domino).first != last_number && 
-        MAP_NUMBER_VALUES.at(domino).second != last_number) {
-            throw std::invalid_argument("last_number != domino.first OR .second");
-        }
         
         dominoes[avaible] = domino;
         last_numbers[avaible] = last_number;
@@ -48,7 +79,6 @@ struct GlobalState {
     int back() {
         return last_numbers[avaible-1];
     }
-
 
     void count(int level) {
         count_[level-1]++;
@@ -69,10 +99,8 @@ struct GlobalState {
 
 
     void pop() {
-        if (avaible) {
-            bank[dominoes[avaible-1]] = 0;
-            --avaible;
-        }
+        bank[dominoes[avaible-1]] = 0;
+        --avaible;
     }
 };
 
@@ -80,19 +108,22 @@ struct GlobalState {
 
 void rec_run_and_count(GlobalState& st,  int max_level=7, int level=1) {
     st.count(level);
+    if (level >= max_level) return;
 
-    if (level >= max_level) {
-        return;
-    }
+    // [0, 1, 0, 0, ... ] = bank
+    // [1, 0, 1, 1, ... ] = ~bank
+    // [1, 0, 0, 1, ... ] = MAP
+    // [1, 0, 0, 1, ... ] = MAP & ~bank : avaible nodes
 
-    for (int next: MAP_VALUE_NUMBER.at(st.back())) {
-        if (!st.bank[next])  {
-            st.add(next);
-            rec_run_and_count(st, max_level, level+1);
-            st.pop();
-        } 
+    auto next_moves = ~st.bank & MAP_VALUE_NUMBER.at(st.back());
+    for (size_t pos = next_moves._Find_first(); pos < 28; pos = next_moves._Find_next(pos)) {
+        st.add(pos); 
+        rec_run_and_count(st, max_level, level+1);
+        st.pop();
     }
 }
+
+
 
 void rec_run_and_save(GlobalState& st, 
                         std::vector<GlobalState>& states_holder,  
@@ -103,15 +134,23 @@ void rec_run_and_save(GlobalState& st,
         states_holder.push_back(st);
         return;
     }
+    
+    // [0, 1, 0, 0, ... ] = bank
+    // [1, 0, 1, 1, ... ] = ~bank
+    // [1, 0, 0, 1, ... ] = MAP
+    // [1, 0, 0, 1, ... ] = MAP & ~bank : avaible nodes
 
-    for (int next: MAP_VALUE_NUMBER.at(st.back())) {
-        if (!st.bank[next])  {
-            st.add(next);
+    auto next_moves = ~st.bank & MAP_VALUE_NUMBER.at(st.back());
+    size_t pos = next_moves._Find_first();
+    
+    while (pos - 28 != 0)  {
+        st.add(pos); 
             rec_run_and_save(st, states_holder,  max_level, level+1);
-            st.pop();
-        } 
+        st.pop();
+        pos = next_moves._Find_next(pos);
     }
 }
+
 
 std::vector<GlobalState> create_first_states(int domino, int last_number, int depth=2) {
 	GlobalState st(domino, last_number);
@@ -121,7 +160,6 @@ std::vector<GlobalState> create_first_states(int domino, int last_number, int de
 }
 
 void print_stat(const std::vector<unsigned long long>& count, int level, int max_depth=28) {
-
     for (int i = level; i < max_depth; ++i) {
         std::cout << i+1 << " : " << count[i] << "\n";
     }
@@ -129,42 +167,14 @@ void print_stat(const std::vector<unsigned long long>& count, int level, int max
 }
 
 
-
-bool some_threads_works(const std::vector<std::thread>& thread_holder) {
-    for (auto& t : thread_holder) {
-        if (t.joinable()) {
-            return true;
-        }
-    }
-    return false;
-}
-
-
-void log_function(const std::vector<std::thread>& thread_holder, std::vector<GlobalState>& states, int level_current, int max_depth) {
-    int size = states.size();
-    while (some_threads_works(thread_holder)) {
-        std::vector<unsigned long long> curr_count(28, 0);
-        for (int i = 0; i < size; ++i) {
-            for (int j = level_current; j <= max_depth; ++j) {
-                curr_count[j-1] += states[i].get_count(j);
-            }    
-        }
-
-        print_stat(curr_count, level_current);
-        std::this_thread::sleep_for(300ms);
-    }
-}
-
-
-
 int main(int argc, char** argv) {
     srand(time(0));
-
 	if (argc == 1) return 1;
 	
 
     int rand_number_dom = rand() % 28;
-	int max_depth = std::atoi(argv[1]);
+	
+    int max_depth = std::atoi(argv[1]);
 	
 	if (max_depth == 1) return 1;
 
@@ -173,33 +183,40 @@ int main(int argc, char** argv) {
     std::cout << "Start number domino : " << rand_number_dom <<  "\n";
     std::cout << "Max Depth :" << max_depth << "\n";
 
+    if (argc == 2) {
+        std::cout << "Add param level_current: 2lvl = 6threads, 3lvl = 35/36 threads ...\n";
+        return 1;
+    }
+
     int level_current = std::atoi(argv[2]); // 2 level == 6 threads, 3 level = 35(or36) threads 
 
 	std::vector<GlobalState> states = create_first_states(rand_number_dom, last_number, level_current);
-    std::cout << states.size() << "\n";
+    std::cout << "Num of States (threads): " << states.size() << "\n";
+
+
 	std::vector<std::thread> thread_holder;
-    
+
+    Timer timer;
+    timer.start();
+
 	for (GlobalState& state : states) {
 		thread_holder.push_back(std::thread(std::ref(rec_run_and_count), std::ref(state), std::ref(max_depth), level_current));
 	}
 
-	std::jthread thread_logger(log_function, std::ref(thread_holder), std::ref(states), level_current, max_depth);
-    
-
-    std::vector<unsigned long long> total_count(28, 0);
+    std::vector<unsigned long long> all_count(28, 0);
 
     int i = 0;
-
-    for (auto& thr : thread_holder) {
-        thr.join();         
+	for (auto& thr : thread_holder) {
+		thr.join();
         for (int j = level_current; j <= max_depth; ++j) {
-            total_count[j-1] += states[i].get_count(j);
+            all_count[j-1] += states[i].get_count(j);
         }
         ++i;
     }
 
+    timer.finish();
 
-    std::cout << "TOTAL SUM : \n";
-    print_stat(total_count, level_current);
+    std::cout <<  "Time : " << timer() << "ms " << "\n";
+    print_stat(all_count, level_current, max_depth);
     return 0;
 }
